d6a2e93cbae5354d634a736c716b7c8ebf0e2755
lAlissial <alissialima@hotmail.com>
2021-11-04 03:28:33 -0300

    Initial commit

---
+++ aplicacao/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Prática | Hello World Angular';
+  nome = 'Alíssia Deolinda Oliveira de Lima';
+}
